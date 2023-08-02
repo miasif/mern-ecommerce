b2e1dfd34f30cd9eaae010bb7c12c64546444164
miasif <minhazul.islam.asif@gmail.com>
2023-08-03 04:04:16 +0600

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api working");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
