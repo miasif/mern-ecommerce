6e584331988fbc89830af43856da27a55d1d406a
miasif <minhazul.islam.asif@gmail.com>
2023-08-14 02:38:38 +0600

    user delete functionality complete

--- frontend/src/pages/admin/UserListPage.jsx
+++ frontend/src/pages/admin/UserListPage.jsx
@@ -4,15 +4,29 @@ import { Table, Button } from "react-bootstrap";
 import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import { useGetUsersQuery } from "../../redux/slices/userSlice";
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+} from "../../redux/slices/userSlice";
 import { toast } from "react-toastify";
 
 const UserListPage = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+  const [deleteUser] = useDeleteUserMutation();
 
   console.log(users);
 
-  const deleteHandler = async (id) => {};
+  const deleteHandler = async (id) => {
+    if (window.confirm("Are you sure")) {
+      try {
+        await deleteUser(id);
+        refetch();
+        toast.success("User Deleted Successfully");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
 
   return (
     <>
