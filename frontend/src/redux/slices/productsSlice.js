39da6757a1d4652d748b50f8620987e54ab01138
miasif <minhazul.islam.asif@gmail.com>
2023-08-17 01:07:57 +0600

    createReview create in redux

--- frontend/src/redux/slices/productsSlice.js
+++ frontend/src/redux/slices/productsSlice.js
@@ -47,6 +47,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         method: "DELETE",
       }),
     }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Product"],
+    }),
   }),
 });
 
@@ -57,4 +65,5 @@ export const {
   useUpdateProductMutation,
   useUploadProductImageMutation,
   useDeleteProductMutation,
+  useCreateReviewMutation,
 } = productsApiSlice;
