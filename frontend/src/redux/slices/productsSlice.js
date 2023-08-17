27b98887de84d5fd53d6c1cd3f6bfebe84de4a21
miasif <minhazul.islam.asif@gmail.com>
2023-08-18 03:00:18 +0600

    getTopProducts create in redux

--- frontend/src/redux/slices/productsSlice.js
+++ frontend/src/redux/slices/productsSlice.js
@@ -55,6 +55,10 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["Product"],
     }),
+    getTopProducts: builder.query({
+      query: () => `${PRODUCTS_URL}/top`,
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -66,4 +70,5 @@ export const {
   useUploadProductImageMutation,
   useDeleteProductMutation,
   useCreateReviewMutation,
+  useGetTopProductsQuery,
 } = productsApiSlice;
